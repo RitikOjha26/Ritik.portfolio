efcdfa121620df8da43aaab6c0a97aebf7e64608
Ritik Ojha <144144477+RitikOjha26@users.noreply.github.com>
2025-05-14 01:54:29 +0530

    Update index.ts
    Updating Resume Link

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 const MAIL_ID = "ojha.ritik99680@gmail.com"
-const RESUME_LINK = 'https://drive.google.com/file/d/1-3Sdkm7xMFxWFgUxBHZ39PGPaQZ5pNQT/view?usp=sharing'
+const RESUME_LINK = 'https://drive.google.com/file/d/1On4aBjQMhH2QNOthQVr3HewkvkMaamgS/view?usp=sharing'
 export default {MAIL_ID, RESUME_LINK}
