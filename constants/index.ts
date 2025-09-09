a5ea4848634293d265765dca28dc27d190e8a385
Ritik Ojha <144144477+RitikOjha26@users.noreply.github.com>
2025-09-10 01:09:36 +0530

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 const MAIL_ID = "ritik.99680@gmail.com"
-const RESUME_LINK = 'https://drive.google.com/file/d/1z7a4jdOV1c1-uTHDphOIM7b_F7g-giAL/view'
+const RESUME_LINK = 'https://drive.google.com/file/d/1hvS2W9DjGDWjdDAgSEnNdnOx_Fzh53Kl/view'
 export default {MAIL_ID, RESUME_LINK}
